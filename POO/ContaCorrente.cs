0ca5f4b979d9ea95758b5275b3cfa6570561b5f1
Johnatan Tavares <johnatan12014@hotmail.com>
2017-12-16 11:37:06 -0200

    Classes

---
+++ POO/ContaCorrente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class ContaCorrente
+    {
+        private int agencia;
+        private int numeroConta;
+        private decimal saldo;
+
+        //Função de Sacar
+        public void Sacar(decimal valor)
+        {
+            saldo = saldo - valor;
+        }
+
+        //Função de Depositar
+        public void Depositar(decimal valor)
+        {
+            saldo = saldo + valor;
+        }
+
+
+    }
+}
